187956628f3d8eeb54973e8fcbd89db05ae3ce76
罗志强 <294052929@qq.com>
2016-11-23 00:03:17 +0800

    啥都没加

--- web/server.go
+++ web/server.go
@@ -1,13 +1,41 @@
 package web
 
 import (
+	"io"
+
 	"github.com/go-xorm/xorm"
 	"github.com/labstack/echo"
 
+	"net/http"
+
+	"path/filepath"
+
+	"github.com/flosch/pongo2"
 	_ "github.com/go-sql-driver/mysql"
 )
 
 var engine *xorm.Engine
+
+//Template 模板
+type Template struct {
+}
+
+//Render 渲染html
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	filename, err := filepath.Abs(name)
+	if err != nil {
+		return err
+	}
+	tmpl, err := pongo2.FromFile(filename)
+	if err != nil {
+		return err
+	}
+	var ctx pongo2.Context
+	if data != nil {
+		ctx = data.(map[string]interface{})
+	}
+	return tmpl.ExecuteWriter(ctx, w)
+}
 
 func main() {
 	var err error
@@ -19,6 +47,11 @@ func main() {
 
 	e := echo.New()
 	e.Static("static", "static")
-	e.Static("/", "webapp")
+	e.GET("/", index)
+	e.Start(":8080")
+}
 
+func index(c echo.Context) error {
+
+	return c.HTML(http.StatusOK, "webapp/index.html")
 }
